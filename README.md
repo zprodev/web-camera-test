86b4a0c98eace26a6b01125d138f68f5beb47f89
zprodev <zproject.develop@gmail.com>
2020-08-20 01:44:19 +0900

    CanvasElementへのマッピング例

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # web-camera-test
 
 ## [VideoElementで表示](https://zprodev.github.io/web-camera-test/video-element/)
+カメラの縦横サイズを気にしない実装。
 
-## [VideoElementで表示（オプションでサイズ指定）](https://zprodev.github.io/web-camera-test/video-element-set-size-option/)
+## [VideoElementでサイズ指定表示](https://zprodev.github.io/web-camera-test/video-element-set-size-option/)
+getUserMediaでカメラの縦横サイズを指定する実装。
+
+## [CanvasElementでサイズ指定表示](https://zprodev.github.io/web-camera-test/canvas-element/)
+getUserMediaでのサイズ指定が効かない場合の代用実装。
+VideoElementからCanvasElementにサイズ指定でマッピングする。
---
+++ canvas-element/main.js
@@ -0,0 +1,58 @@
+
+const videoElement = document.createElement('video');
+videoElement.width = 300;
+videoElement.height = 300;
+videoElement.setAttribute('playsinline', '');
+videoElement.muted = true;
+videoElement.style.backgroundColor = '#000000';
+document.body.appendChild(videoElement);
+
+const canvasElement = document.createElement('canvas');
+const canvasContext = canvasElement.getContext('2d')
+canvasElement.width = 300;
+canvasElement.height = 300;
+canvasElement.style.backgroundColor = '#000000';
+document.body.appendChild(canvasElement);
+
+let mediaStream = null;
+
+const videoOption = {
+  facingMode: 'environment',
+};
+
+window.navigator.mediaDevices.getUserMedia({
+  video: videoOption,
+  audio: false,
+}).then((_mediaStream) => {
+  mediaStream = _mediaStream;
+  videoElement.srcObject = _mediaStream;
+  videoElement.play();
+}).catch((error) => {
+  alert(error.message);
+});
+
+(function render() {
+  requestAnimationFrame(render);
+  if (videoElement.readyState !== videoElement.HAVE_ENOUGH_DATA) return;
+  const mediaTrackSettings = mediaStream.getVideoTracks()[0].getSettings();
+
+  let sourceX = 0;
+  let sourceY = 0;
+  let sourceWidth = mediaTrackSettings.width;
+  let sourceHeight = mediaTrackSettings.height;
+  const canvasAspectRatio = canvasElement.width / canvasElement.height;
+  if (mediaTrackSettings.aspectRatio > canvasAspectRatio) {
+    // 左右トリミング
+    sourceX = (mediaTrackSettings.width - mediaTrackSettings.height) * 0.5;
+    sourceWidth = mediaTrackSettings.height;
+  } else if (mediaTrackSettings.aspectRatio < canvasAspectRatio) {
+    // 上下トリミング
+    sourceY = (mediaTrackSettings.height - mediaTrackSettings.width) * 0.5;
+    sourceHeight = mediaTrackSettings.width;
+  }
+
+  canvasContext.drawImage(videoElement,
+    sourceX, sourceY, sourceWidth, sourceHeight,
+    0, 0, canvasElement.width, canvasElement.height
+  );
+}());
